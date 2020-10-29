e5b899e3b04ec0210427e3a63b6ae6c95b182ee6
Emil Skogsund <skogix@gmail.com>
2020-10-29 20:13:42 +0100

    parsechar snyggare

--- Parser/Library.fs
+++ Parser/Library.fs
@@ -15,32 +15,13 @@ module Basics =
   /// med curry
   /// char -> str -> Parser<char>
   let parseChar (charToParse:char) =
-    // todo: jävla fult att göra till array och tillbaka till list, kolla upp snyggare lösning
     let f (str:string) =
-      str.ToCharArray()
-      |> Array.toList
-      |> function
+      match Seq.toList str with
         | first::rest ->
-          if first = charToParse then Success(charToParse, rest.ToString())
+          if first = charToParse then Success(charToParse, rest |> string)
           else Failure(sprintf "Ville ha %c, fick %c" charToParse first)
         | [] -> Failure ("Ingen mer input")
     Parser f
-(*
-    // currya med inre funktion
-    // todo: kanske mer funktionellt att göra en match first::rest / []
-    let f (str:string) =
-      // om dem ska gå att chaina så kan det komma tom input
-      if String.IsNullOrEmpty(str) then
-        Failure "Ingen mer input"
-      else
-        let first = str.[0]
-        if first = charToParse then
-          let rest = str.[1..]
-          Success (charToParse, rest)
-        else
-          Failure (sprintf "Ville ha %c, fick %c" charToParse first)
-    Parser f
-*)
   /// "unwrapper" för parser, kör i princip sett bara inre funktionen i parsern som passats
   let run (parser:Parser<'a>) (input:string): Result<'a * string> =
     // deconstructar parser precis som en (x,y) skulle deconstructa en tuple
