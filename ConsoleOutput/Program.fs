0c50a72f7375a794124f6c1468e3ecf69dc7fbce
Emil Skogsund <skogix@gmail.com>
2020-10-29 19:15:35 +0100

    parser init

--- ConsoleOutput/Program.fs
+++ ConsoleOutput/Program.fs
@@ -1,8 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Parser
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main _ =
+    Say.hello "Skogix"
     0 // return an integer exit code
