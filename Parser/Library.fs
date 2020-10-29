9957eef8db7f3d8a375c36a38266bb2193ba761f
Emil Skogsund <skogix@gmail.com>
2020-10-29 23:24:43 +0100

    comments

--- Parser/Library.fs
+++ Parser/Library.fs
@@ -4,14 +4,11 @@ open System
 type Result<'a> =
   | Success of 'a
   | Failure of string
-/// en map till parser, funkar som en vanlig wrapper
-/// för string -> Result<'T * string> till Parser<'T>
+/// Wrapper som mappar string till result
 /// returnar antingen parsertypen + rest eller failmsg + rest
 type Parser<'T> = Parser of (string -> Result<'T * string>)
 /// parsea en char från en str och mappa till parser
-/// char -> Parser<char>
-/// med curry
-/// char -> str -> Parser<char>
+/// har en str curryad
 let parseChar charToParse =
   let f (str:string) =
     if String.IsNullOrEmpty(str) then
@@ -24,24 +21,19 @@ let parseChar charToParse =
       else
         Failure (sprintf "Ville ha %c, fick %c" charToParse first)
   Parser f
-/// "unwrapper" för parser, kör i princip sett bara inre funktionen i parsern som passats
+/// "unwrapper" för parser, kör inre funktionen
 let run (parser) (input) =
   // deconstructar parser precis som en (x,y) skulle deconstructa en tuple
   let (Parser runInnerFunction) = parser
   runInnerFunction input
 
 /// and-combinator
-/// vill kunna >> två parsers, typ parseA >> parseB
-/// så se till att output och input är samma och "låta fails" gå förbi
+/// båda måste matcha för success return
 /// todo kolla bind-lösning
-///
-/// kör första parsern
-///   om fail, return
-/// kör andra parsern med rest
-///   om fail, return
-/// om ingen fail, returna en tuple med båda parseade values
-
 let andThen parser1 parser2 =
+  /// if error, break
+  /// elseif error, break
+  /// return båda
   let f input =
     let result1 = run parser1 input
     match result1 with
@@ -58,14 +50,7 @@ let andThen parser1 parser2 =
 let ( .>>. ) = andThen
 
 /// or-combinator
-/// matcha a eller b
-///
-/// kör första
-///   om success, returna parseade och rest
-/// annars on fail
-///   kör andra parsern med orginalinput
-///   returna
-
+/// a eller b måste success för return
 let orElse parser1 parser2 =
   // kör 1, return om success
   // annars return 2
@@ -79,7 +64,6 @@ let orElse parser1 parser2 =
   Parser f
 /// infix för orElse
 let ( <|> ) = orElse
-
 /// choice 
 /// kör orElse över en hel lista med combinators för att få choice
 ///
@@ -88,9 +72,7 @@ let ( <|> ) = orElse
 /// sista om alla failat
 let choice listOfParsers =
   List.reduce (<|>) listOfParsers
-
 /// anyOf
-/// ha en lista med alla bokstäver, transforma alla till parsers
 /// combinea alla med choice
 let anyOf listOfChars =
   listOfChars
